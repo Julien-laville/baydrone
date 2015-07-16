ff1f13dc05ef09c50388c578c249d3914d786f7a
julien <alternative451@gmail.com>
2015-07-16 08:59:12 +0200

    first crawl

--- routes/admin/collect.js
+++ routes/admin/collect.js
@@ -12,6 +12,22 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/1/start', function(req, res, next) {
+
+
+    var crawler = new Crawler({
+        callback : function(err, body, jquery) {
+            var arianes = [];
+            jquery('.defaultLineHeight a').each(function(i, ariane) {
+                var arianeLink = jquery(ariane).text();
+                arianes.push(arianeLink);
+            });
+            res.json(arianes);
+        }
+    });
+    crawler.queue('http://www.hobbyking.com/hobbyking/store/__78045__Basher_RZ_4_1_10_Rally_Racer_ARR_UK_Warehouse_.html')
+});
+
 router.get('/new', function(req, res, next) {
     res.render('admin/collect/new', {})
 });
@@ -22,15 +38,6 @@ router.get('/:id', function(req, res, next) {
     });
 });
 
-router.get(':id/start', function(req, res, next) {
-    var crawler = new Crawler({
-        callback : function(err, res, jquery) {
-            jquery('.defaultLineHeight').each(function(i, ariane) {
-                console.log(ariane);
-            })
-        }
-    });
-    crawler.queue('http://www.hobbyking.com/hobbyking/store/__78045__Basher_RZ_4_1_10_Rally_Racer_ARR_UK_Warehouse_.html')
-});
+
 
 module.exports = router;
